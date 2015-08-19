f6880901710424197ea819b5393c48e48d106195
Soham Banik <soham.banik14@gmail.com>
2015-08-19 14:49:32 +0530

    Second commit

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// MongoDB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
+require('./models/Posts');
+require('./models/Comments');
+
+// Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -55,6 +62,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
